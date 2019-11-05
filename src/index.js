37e214b788a2488d72bd7255338e65347b7c9dc4
klucsik <klucsik.krisztian@gmail.com>
2019-11-05 23:29:14 +0100

    errors and conditional rendering
    added error boundaries to catch those nasty error messages and added some conditional rendering

--- src/index.js
+++ src/index.js
@@ -2,59 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import SearchForm from "./Search_and_list";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-  useParams
-} from "react-router-dom";
+import HulladekCard from "./Card";
+import ErrorBoundary from "./errors";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.handleHullSelect = this.handleHullSelect.bind(this);
-    this.state = { searchterm: "első" };
+    this.state = {
+      appstate: "search",
+      searchterm: "első",
+
+      selected_hullinfo: {
+        alias_list: ["blokk", "hőpapír"],
+        hull_id: 25,
+        name: "Alma",
+        picurl: "/static/images/elso.jpg"
+      }
+    };
   }
 
   handleSearchChange(searchterm) {
     this.setState({ searchterm });
   }
   handleHullSelect(hullinfo) {
-    alert("felért" + hullinfo);
+    this.setState({ selected_hullinfo: hullinfo, appstate: "hullinfo" });
   }
   render() {
-    if (this.state.redirect) {
-      return <Redirect push to="/sample" />;
-    }
     return (
-      <Router>
-        <div className="App">
-          előző keresés: {this.state.searchterm}
+      <div className="App">
+        <p>előző keresés: {this.state.searchterm}</p>
+        <p>megjelenő hulladék: {this.state.selected_hullinfo.name}</p>
+        {this.state.appstate === "search" && (
           <SearchForm
             onSearchSubmit={this.handleSearchChange}
             onHullSelect={this.handleHullSelect}
           />
-          <Switch>
-            <Route path="/:id" children={<Child />} />
-          </Switch>
-        </div>
-      </Router>
+        )}
+        <ErrorBoundary>
+          {this.state.appstate === "hullinfo" && (
+            <HulladekCard hullinfo={this.state.selected_hullinfo} />
+          )}
+        </ErrorBoundary>
+      </div>
     );
   }
-}
-
-function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
-  let { id } = useParams();
-
-  return (
-    <div>
-      <h3>ID: {id}</h3>
-    </div>
-  );
 }
 
 const rootElement = document.getElementById("root");
